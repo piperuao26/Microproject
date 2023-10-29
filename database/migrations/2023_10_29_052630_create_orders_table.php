2c6f6dc5f7246dca67534a6fbf13fb58afc9e352
piperuao26 <piperuao26@gmail.com>
2023-10-29 00:28:02 -0500

    Creacion migracion de order

---
+++ database/migrations/2023_10_29_052630_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // Para relacionar con el usuario
+            $table->dateTime('order_date'); // Fecha del pedido
+            $table->decimal('total_price', 10, 2); // Precio total del pedido
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
+
