e88473acc2d55223859524fa3ffd594c5b9d0e6e
piperuao26 <piperuao26@gmail.com>
2023-10-31 17:47:03 -0500

    Login controller falta terminarlo

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginController extends Controller
+{
+    //
+    public function register(Request $request){
+
+    }
+
+    public function loging(Request $request){
+
+    }
+
+    public function logout(Request $request){
+
+    }
+
+    
+}
